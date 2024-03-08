48836f53b898c47233f2015aeb73755473b9e7f2
RehyanR <151837449+RehyanR@users.noreply.github.com>
2024-03-08 19:21:53 +0000

    added first question to questions.js

---
+++ assets/js/questions.js
@@ -0,0 +1,8 @@
+// Questions and answers
+const questions = [
+  {
+    question: "What Does HTML Stand For?",
+    choices: ["1. HighText Machine Language", "HyperText and links Markup Language", "HyperText Markup Language", "HyperText Mixed Language"],
+    answer: "HyperText Markup Language"
+  },
+]
