5531a64aaeafa1d1890f200314f6fbfe4dec5eed
RehyanR <151837449+RehyanR@users.noreply.github.com>
2024-03-08 19:45:58 +0000

    added all 5 questions to logic.js file

---
+++ assets/js/logic.js
@@ -0,0 +1,53 @@
+// Questions and answers
+const questions = [
+    {
+      question: "What Does HTML Stand For?",
+      choices: [
+        "HighText Machine Language",
+        "HyperText and links Markup Language",
+        "HyperText Markup Language",
+        "HyperText Mixed Language",
+      ],
+      answer: "HyperText Markup Language",
+    },
+    {
+      question: "What Does CSS Stand For?",
+      choices: [
+        "Cascade Style Sheets",
+        "Colour and Style Sheets",
+        "Complete Style Sheets",
+        "Cascading Style Sheets",
+      ],
+      answer: "Cascading Style Sheets",
+    },
+    {
+      question: "What is an API?",
+      choices: [
+        "Application Programming Interface",
+        "Application Profile Interface",
+        "Application Portfolio Interface",
+        "Application Public Interface",
+      ],
+      answer: "Application Programming Interface",
+    },
+    {
+      question: "What Does DOM Stand For?",
+      choices: [
+        "Document Observe Model",
+        "Document Object Model",
+        "Document Option Model",
+        "Document Open Model",
+      ],
+      answer: "Document Object Model",
+    },
+    {
+      question: "Which of the following is a client-side storage mechanism in web browsers that allows websites to store data persistently on a user's device?",
+      choices: [
+        "Local Storage",
+        "PHP Sessions",
+        "Cookies",
+        "Session Storage",
+      ],
+      answer: "Local Storage",
+    },
+  ];
