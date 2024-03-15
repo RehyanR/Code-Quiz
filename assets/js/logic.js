00cf056e729bf20cf1e0182dacbe8b5a6732c488
RehyanR <151837449+RehyanR@users.noreply.github.com>
2024-03-15 03:13:19 +0000

    formatted code

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,58 +1,54 @@
 // Questions and answers
 const question = [
-    {
-      question: "What Does HTML Stand For?",
-      choices: [
-        "HighText Machine Language",
-        "HyperText and links Markup Language",
-        "HyperText Markup Language",
-        "HyperText Mixed Language",
-      ],
-      answer: "HyperText Markup Language",
-    },
-    {
-      question: "What Does CSS Stand For?",
-      choices: [
-        "Cascade Style Sheets",
-        "Colour and Style Sheets",
-        "Complete Style Sheets",
-        "Cascading Style Sheets",
-      ],
-      answer: "Cascading Style Sheets",
-    },
-    {
-      question: "What is an API?",
-      choices: [
-        "Application Programming Interface",
-        "Application Profile Interface",
-        "Application Portfolio Interface",
-        "Application Public Interface",
-      ],
-      answer: "Application Programming Interface",
-    },
-    {
-      question: "What Does DOM Stand For?",
-      choices: [
-        "Document Observe Model",
-        "Document Object Model",
-        "Document Option Model",
-        "Document Open Model",
-      ],
-      answer: "Document Object Model",
-    },
-    {
-      question: "Which of the following is a client-side storage mechanism in web browsers that allows websites to store data persistently on a user's device?",
-      choices: [
-        "Local Storage",
-        "PHP Sessions",
-        "Cookies",
-        "Session Storage",
-      ],
-      answer: "Local Storage",
-    },
-  ];
+  {
+    question: "What Does HTML Stand For?",
+    choices: [
+      "HighText Machine Language",
+      "HyperText and links Markup Language",
+      "HyperText Markup Language",
+      "HyperText Mixed Language",
+    ],
+    answer: "HyperText Markup Language",
+  },
+  {
+    question: "What Does CSS Stand For?",
+    choices: [
+      "Cascade Style Sheets",
+      "Colour and Style Sheets",
+      "Complete Style Sheets",
+      "Cascading Style Sheets",
+    ],
+    answer: "Cascading Style Sheets",
+  },
+  {
+    question: "What is an API?",
+    choices: [
+      "Application Programming Interface",
+      "Application Profile Interface",
+      "Application Portfolio Interface",
+      "Application Public Interface",
+    ],
+    answer: "Application Programming Interface",
+  },
+  {
+    question: "What Does DOM Stand For?",
+    choices: [
+      "Document Observe Model",
+      "Document Object Model",
+      "Document Option Model",
+      "Document Open Model",
+    ],
+    answer: "Document Object Model",
+  },
+  {
+    question:
+      "Which of the following is a client-side storage mechanism in web browsers that allows websites to store data persistently on a user's device?",
+    choices: ["Local Storage", "PHP Sessions", "Cookies", "Session Storage"],
+    answer: "Local Storage",
+  },
+];
 
-  // Function to start the quiz
+// Function to start the quiz
 function startQuiz() {
   document.getElementById("start").style.display = "none";
   document.getElementById("questions").classList.remove("hide");
