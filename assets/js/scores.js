a4971aefd6a2ac780dd6e3797a87bad3a04a09a8
RehyanR <151837449+RehyanR@users.noreply.github.com>
2024-03-17 11:11:51 +0000

    added function to retrieve initals and highscore

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -2,3 +2,8 @@ function saveScore(initials, topScore) {
   localStorage.setItem("initials", initials);
   localStorage.setItem("topScore", topScore);
 }
+function getScore() {
+    const initials = localStorage.getItem('initials') || '';
+    const topScore = localStorage.getItem('topScore') || 0;
+    return { initials, topScore };
+  }
